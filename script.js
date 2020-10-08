a1c20767efbb63e0828b760106a27e092bb3d7f4
Ali <ansarialiraza193@gmail.com>
2020-10-08 16:06:33 +0500

    made it!

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let elem = document.getElementsByTagName("h2");
 function randomNumberGenerator(min, max) {  
     return Math.floor(Math.random() * (max - min) + min); 
 }
@@ -7,8 +6,25 @@ let small = letters.split('');
 let cap = letters.toUpperCase().split('');
 let symbols = '!@#$%^&*'.split('');
 let randomNumber = randomNumberGenerator(10,100);
-let password;
-let object1 = {
-    
+function generateString() {
+    return (randomNumberGenerator(10, 100) + small[randomNumberGenerator(0, 26)] + cap[randomNumberGenerator(0, 26)] + symbols[randomNumberGenerator(0, 7)] + randomNumberGenerator(100, 1000) + cap[randomNumberGenerator(0, 26)] + cap[randomNumberGenerator(0, 26)] + symbols[randomNumberGenerator(0, 7)]);
 }
-console.log(randomNumber,small, cap, symbols)
+
+let elem = document.getElementById('string');
+
+let passString = generateString();
+elem.innerText = passString;
+
+function reGenerate() {
+    passString = generateString();
+    elem.innerText = passString;
+}
+
+function clickToCopy() {
+    var inp = document.createElement('input');
+    document.body.appendChild(inp);
+    inp.value = passString;
+    inp.select();
+    document.execCommand('copy');
+    inp.remove();
+}
