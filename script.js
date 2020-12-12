bc439d61452f6545d4f2d19cffcc65dd8d8dea73
aliraza5490 <57584928+aliraza5490@users.noreply.github.com>
2020-12-12 15:15:33 +0500

    Adds icons and password length feature

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 function randomNumberGenerator(min, max) {  
     return Math.floor(Math.random() * (max - min) + min); 
 }
-let letters = 'abcdefghijklmnopqrstuvwxyz';
-let small = letters.split('');
-let cap = letters.toUpperCase().split('');
-let symbols = '!@#$%^&*'.split('');
-let randomNumber = randomNumberGenerator(10,100);
-function generateString() {
-    return (randomNumberGenerator(10, 100) + small[randomNumberGenerator(0, 26)] + cap[randomNumberGenerator(0, 26)] + symbols[randomNumberGenerator(0, 7)] + randomNumberGenerator(100, 1000) + cap[randomNumberGenerator(0, 26)] + cap[randomNumberGenerator(0, 26)] + symbols[randomNumberGenerator(0, 7)]);
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const small = letters.split('');
+const cap = letters.toUpperCase().split('');
+const symbols = '!@#$%^&*'.split('');
+const randomNumber = randomNumberGenerator(10,100);
+const passElements = [ letters, small, cap, symbols, randomNumber];
+
+function generatePassword() {
+	let password = [];
+	const lengthElementValue = Number(document.getElementById("length").value);
+	if(!lengthElementValue&&lengthElementValue<8){
+		alert("Length value is required and should be greater than 8.");
+	}else{
+		const lengthOfPassword = lengthElementValue;
+		for (let i=1;i<=lengthOfPassword;i++){
+			//tempIndex is equal to random index number of passElements
+			tempIndex = randomNumberGenerator(0, passElements.length-1);
+			//length of the random passElement
+			len = passElements[tempIndex].length;
+			password.push(passElements[tempIndex][randomNumberGenerator(0, len)]);
+		}
+		return password.join("");
+	}
+	
 }
 
-let elem = document.getElementById('string');
-
-let passString = generateString();
+const elem = document.getElementById('string');
+document.getElementById("copy-btn").addEventListener( "click", clickToCopy)
+document.getElementById("new-btn").addEventListener( "click", reGenerate)
+let passString = generatePassword();
 elem.innerText = passString;
-
 function reGenerate() {
-    passString = generateString();
+    passString = generatePassword();
     elem.innerText = passString;
 }
 
 function clickToCopy() {
-    var inp = document.createElement('input');
-    document.body.appendChild(inp);
-    inp.value = passString;
-    inp.select();
-    document.execCommand('copy');
-    inp.remove();
+	navigator.clipboard.writeText(passString)
+    .catch(err => console.log('Async writeText failed with error: "' + err + '"'));;
 }
+      
